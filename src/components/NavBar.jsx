7c2d0ba27016386cd1c83892c7af50d56d674eba
ajaypv <75624178+ajaypv@users.noreply.github.com>
2022-09-14 21:42:23 +0530

    created the NavBar.js and NavBar.css in Components floder

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./NavBar.css";
+const NavBar = () =>{
+    return (
+        <>
+      
+        <div className="Container">
+        <h1>
+            logo
+        </h1>
+        <h3 className="NavItems">Home</h3>
+        <h3 className="NavItems">Connect Dots</h3>
+        <h3 className="NavItems">Login</h3>
+        </div>
+        </>
+    )
+}
+
+export default NavBar;
